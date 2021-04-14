751003d80d07bfd72a1e1ef30d1e39cc6f3a32cc
Jammone <gianluca.mammone96@gmail.com>
2021-04-14 09:35:21 +0200

    Update init.m

--- init.m
+++ init.m
@@ -3,36 +3,53 @@
 
 
 %% param setting
-syms q1 q2 dq1 dq2 ddq1 ddq2 real
+syms q1 q2 dq1 dq2 ddq1 ddq2 u1 real
 
-mb = 5;              % bar mass
-MB = 30;             % body mass
-l = 0.5;             % lunghezza asta 
+m1 = 5;              % giunto 1 mass
+m2 = 5;              % giunto 2 mass
+l1 = 0.5;             % lunghezza primo giunto
+lc1 = 0.25;           % centro di massa giunto 1
+l2 = 0.5;
+lc2 = 0.25;
+I1 = 0.05;
+I2 = 0.05;
+
 g = 9.81;            % constante di accelerazione gravitazionale
-I = 0.05;            % inerzia 
 
+PARAM= [m1,m2,I1,lc1,l2,lc2,I1,I2];
 
 %% model without uncertainties
 % M*[ddq1; ddq2] + n = [u;0]
+a1 = I1+ I2+ m1*lc1^2+m2*(l1^2+lc2^2);
+a2 = m2*l1*lc2^2;
+a3 = I2 + m2*lc2^2;
+a4 = g*(m1*lc1+m2*l1);
+a5 = g*m2*lc2;
 
-M = [mb+Mb, m*l*cos(q2);
-     m*l*cos(theta), I+ mb*l^2;
-     ];
-n = [-mb*l*sin(q2)*dq2;
-    -mb+g*l*sin(q2)];
-
+M = [a1+2*a2*cos(q2), a3+a2*cos(q2);
+     a3+a2*cos(q2), a3];
+ 
+c = [-a2*dq2*(dq2+2*dq1)*sin(q2);
+    a2*dq1^2*sin(q2)];
+e = [ a4*sin(q1) + a5*sin(q1+q2);
+    a5*sin(q1+q2)];
+n = c+e;
 % dstate = f(state) + g(state)*u
 state = [q1,q2,dq1,dq2]';
 
 f = [dq1;
     dq2;
-    inv(M)*(mb*l*sin(q2)*dq2);
-    inv(M)*(mb*l*sin(q2))];
+    -inv(M)*n(1);
+    -inv(M)*n(2)];
 
-g =[0;
-    0;
+g =[0,0;
+    0,0;
     inv(M);
-    0];
+    0,0];
 
-dstate = f + g*[u;0];
+dstate = f + g*[u1;0];
+%% Optimal control
+ J= 0.5*norm(u-Kp(-state))^2;
+ 
+ 
 
