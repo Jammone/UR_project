a893861072d47a335cbf4a066f05cd5932832a68
Lorenzo De Rebotti <lorenzo.derebotti@gmail.com>
2021-05-19 09:04:10 +0200

    added cbf

--- lqr_init.m
+++ lqr_init.m
@@ -2,17 +2,20 @@ clc;
 clear;
 close;
 
-m1 = 2;            % giunto 1 mass
-m2 = 2;              % giunto 2 mass
-l1 = 0.3;             % lunghezza primo giunto
-lc1 = 0.15;           % centro di massa giunto 1
-l2 = 0.3;
-lc2 = 0.15;
-I1 = 0.933;
-I2 = 0.933;
+syms q1 q1dot q2 q2dot;
+x = [q1 q1dot q2 q2dot]';
 
-f1 = 1;
-f2 = 1;
+m1 = 1;            % giunto 1 mass
+m2 = 1;              % giunto 2 mass
+l1 = 0.5;             % lunghezza primo giunto
+lc1 = l1/2;           % centro di massa giunto 1
+l2 = 0.5;
+lc2 = l2/2;
+I1 = 0.1;
+I2 = 0.1;
+
+f1 = 0.5;
+f2 = 0.5;
 
 F = diag([f1,f2]);
 
@@ -28,22 +31,20 @@ a5 = g*m2*lc2;
 
 COEFF = [a1 a2 a3 a4 a5 f1 f2];
 
-INITIAL_CONDITIONS = [-pi, 0, 0, 0];
-Q_DES = [pi;0];
-% K_p = [60 0;
-%        0  60];
-% K_d = [12 0; 
-%         0 12];
+INITIAL_CONDITIONS = [pi + pi/12, -pi/12, 0, 0];
+Q1E = pi-pi/12;
 
-q1e = Q_DES(1);
-q2e = Q_DES(2);
+q1e = Q1E;
+q2e = pi - Q1E;
+
+Q_DES = [q1e; q2e];
 
 n = [1 0]';
 
 Q = diag([10 10 1 1]);
 R = 1;
 
-tau_eq = [a4*sin(q1e)+a5*sin(q1e+q2e);0];
+tau_eq = [a4*sin(q1e)+a5*sin(q1e+q2e)];
 
 Me = [a1 + 2*a2*cos(q2e), a3+a2*cos(q2e);
          a3+a2*cos(q2e),          a3];
@@ -54,14 +55,23 @@ He = [a4*cos(q1e)+a5*cos(q1e+q2e),  a5*cos(q1e+q2e);
 A = [zeros(2), eye(2);
      -Me\He,   -Me\F];
 
-B = [0; 0; Me\n];
+b = [0; 0; Me\n];
 
-[K,s,e] = lqrd(A,B,Q,R,0.005);
+[K,s,e] = lqrd(A,b,Q,R,0.005);
 
 K_p = -K(1:2);
 K_d = -K(3:4);
 
-sim("pendubot.slx");
+% CBF definition
+Q1E_MAX = pi/12;
+c = 0.1;
+h = 0.5*(Q1E_MAX^2-(q1-q1e)-c*q1dot);
+
+% QP definition
+lb = -100;
+ub = 100;
+
+sim("lqr_pendubot.slx");
 %plot gragico dinamico avanzato
  fig = figure();
  j1x = l1*sin(INITIAL_CONDITIONS(1));
